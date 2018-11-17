3e3dcb56186a723fb8bc68ae89252ae79e5f1ff4
Ferran Pons <ferranpons@gmail.com>
2018-11-17 14:12:26 +0100

    score converted to timer

--- score.py
+++ score.py
@@ -4,6 +4,7 @@ import pygame
 
 
 class Score(pygame.sprite.Sprite):
+    start_ticks = 0
 
     def __init__(self, score):
         self.score = score
@@ -11,12 +12,11 @@ class Score(pygame.sprite.Sprite):
         self.font = pygame.font.Font(None, 20)
         self.font.set_italic(1)
         self.color = pygame.Color('white')
-        self.lastscore = -1
         self.update()
-        self.rect = self.image.get_rect().move(10, 450)
+        self.rect = self.image.get_rect().move(500, 50)
+        self.start_ticks = pygame.time.get_ticks()
 
     def update(self):
-        if self.score != self.lastscore:
-            self.lastscore = self.score
-            msg = "Score: %d" % self.score
-            self.image = self.font.render(msg, 0, self.color)
+        seconds = (pygame.time.get_ticks() - self.start_ticks) / 10
+        msg = "Time: %d" % seconds
+        self.image = self.font.render(msg, 0, self.color)
