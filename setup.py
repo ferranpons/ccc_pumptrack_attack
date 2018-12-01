6dda330d9d1c811c6ef4ed986c3a861bc2758557
Ferran Pons <ferranpons@gmail.com>
2018-12-01 21:03:19 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
-import cx_Freeze
+from cx_Freeze import setup, Executable
 
-executables = [cx_Freeze.Executable("game.py")]
+include_files = [
+    "data/background_menu.png",
+    "data/background_title.png",
+    "data/ccc_logo.png",
+    "data/ccc_pumptrack_attack_title.png",
+    "data/fps_logo.png",
+    "data/intro_ball.gif",
+    "data/leaderboard.txt",
+    "data/menu_line.png",
+    "data/pumptrack.png"
+    "data/pygame_badge_SMA.png",
+    "data/rider1_placeholder.png",
+    "data/Windom_Earle_07_screw_wave.mp3"
+]
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"], 'include_files': include_files}
 
 cx_Freeze.setup(
     name="CCC Pumptrack Attack",
-    version = "0.1",
-    description = "Open Source video game with a fancy pixel art about riding pump tracks with a mountain bike",
-    options={"build_exe": {"packages": ["pygame"],
-                           "include_files": [
-                               "data/background_menu.png",
-                               "data/background_title.png",
-                               "data/ccc_logo.png",
-                               "data/ccc_pumptrack_attack_title.png",
-                               "data/fps_logo.png",
-                               "data/intro_ball.gif",
-                               "data/leaderboard.txt",
-                               "data/menu_line.png",
-                               "data/pumptrack.png"
-                               "data/pygame_badge_SMA.png",
-                               "data/rider1_placeholder.png",
-                               "data/Windom_Earle_07_screw_wave.mp3"
-                           ]}
-             },
-    executables=executables
-
+    version="0.1",
+    description="Open Source video game with a fancy pixel art about riding pump tracks with a mountain bike",
+    author='Ferran Pons',
+    options={"build_exe": build_exe_options},
+    executables=[Executable(script="game.py", base="Win32GUI", targetName="YASS.exe")]
 )
